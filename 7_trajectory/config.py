505b7bc336b9a11856390febabdbf84e1aa63c26
nmasse <nicolas.masse@gmail.com>
2024-07-16 13:30:18 -0400

    updated params

--- 7_trajectory/config.py
+++ 7_trajectory/config.py
@@ -2,23 +2,21 @@
 cell_properties = {
     "Dementia": {"discrete": True, "values": [0, 1], "stop_grad": False},
     "BRAAK_AD": {"discrete": False, "values": [-1], "stop_grad": False},
-    "Sex": {"discrete": True, "values": ["Male", "Female"], "stop_grad": True},
-    "Brain_bank": {"discrete": True, "values": ["MSSM", "RUSH"], "stop_grad": True},
-    "Age": {"discrete": False, "values": [-1], "stop_grad": True},
-    "PMI": {"discrete": False, "values": [-1], "stop_grad": True},
     "SubID": {"discrete": True, "values": None,  "stop_grad": True},
 }
 
 batch_properties = None
 
 dataset_cfg = {
-    "data_path": None, # this needs to be changed to location of processed dataset (.dat file)
-    "metadata_path": None, # this needs to be changed to location of metadata (.pkl file)
+    "data_path": "/home/masse/work/data/mssm_rush_v2/data1.dat",
+    "metadata_path": "/home/masse/work/data/mssm_rush_v2/metadata.pkl",
     "cell_properties": cell_properties,
     "batch_size": 512,
-    "num_workers": 8,
+    "num_workers": 14,
     "batch_properties": batch_properties,
     "remove_sex_chrom": True,
+    "protein_coding_only": True,
+    "top_k_genes": 25_000,
     "cell_restrictions": {"class": "Astro"},
     "mixup": False,
     "group_balancing": "bd",
@@ -28,28 +26,27 @@ model_cfg = {
     "n_layers": 2,
     "n_hidden": 512,
     "n_hidden_decoder": 512,
-    "n_hidden_library": 64,
     "n_latent": 32,
     "n_latent_cell_decoder": 32,
     "dropout_rate": 0.5,
-    "input_dropout_rate": 0.5,
+    "input_dropout_rate": 0.25,
+    "chromosome_dropout": False,
+    "standard_dropout": True,
     "grad_reverse_dict": {
-        "SubID": 0.2, "Brain_bank": 0.2, "Age": 0.2, "Sex": 0.2, "PMI": 0.2,
+        "SubID": 0., "Age": 0., "Sex": 0., "Brain_bank": 0., "PMI": 0.,
     },
     "cell_decoder_hidden_layer": False,
 }
 
 task_cfg = {
-    "learning_rate": 5e-4,
+    "learning_rate": 2e-4,
     "warmup_steps": 2000.0,
-    "weight_decay": 0.0,
+    "weight_decay": 0.05,
     "l1_lambda": 0.0,
-    "gene_loss_coeff": 2e-3,
+    "gene_loss_coeff": 1e-3,
     "balance_classes": False,
     "n_epochs_kl_warmup": None,
     "batch_properties": batch_properties,
-    "save_gene_vals": False,
-    "use_gdro": False,
 }
 
 trainer_cfg = {
