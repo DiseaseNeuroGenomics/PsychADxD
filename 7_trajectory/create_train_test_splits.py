38cfb157ce5f43582dc173f538960e7bf3884bf2
nmasse <nicolas.masse@gmail.com>
2025-07-09 11:19:51 -0400

    clean up, and cutting unnecessary fields

--- 7_trajectory/create_train_test_splits.py
+++ 7_trajectory/create_train_test_splits.py
@@ -3,24 +3,29 @@ import numpy as np
 
 class Splits:
 
-    def __init__(self, metadata_fn: str, save_fn: str, n_splits: int = 10):
+    """Code to generate train/test splits, required for model training.
+    Splits are chosen so that the same donor is never in both the train and test splits."""
 
+    def __init__(self, metadata_fn: str, save_fn: str, n_splits: int = 10, seed: int = 42):
+
+        np.random.seed(seed)
         self.meta = pickle.load(open(metadata_fn, "rb"))
         self._subset_data()
         self.save_fn = save_fn
         self.n_splits = n_splits
         self.splits = {}
 
-    def _subset_data(self, age_threshold: float = 65.0):
+    def _subset_data(self, age_threshold: float = 0.0):
 
-        # idx = self.meta["obs"]["Age"] >= age_threshold
-        self.cell_idx = np.where(self.meta["obs"]["include_training"])[0]
+        self.cell_idx = np.where(self.meta["obs"]["Age"] > age_threshold)[0]
 
     def create_splits(self):
 
         self.splits = {}
+        self.splits_donor = {}
         self._split_by_subjects()
         self._add_indices()
+
         pickle.dump(self.splits, open(self.save_fn, "wb"))
 
     def _split_by_subjects(self):
@@ -32,7 +37,7 @@ class Splits:
         test_subject_splits = np.array_split(subjects, self.n_splits)
         for n in range(self.n_splits):
             train_subject_split = list(set(subjects) - set(test_subject_splits[n]))
-            self.splits[n] = {
+            self.splits_donor[n] = {
                 "train_subjects": train_subject_split,
                 "test_subjects": test_subject_splits[n].tolist(),
             }
@@ -42,27 +47,28 @@ class Splits:
         for i in range(self.n_splits):
 
             print(f"Split number {i}")
+            self.splits[i] = {}
 
             idx = [
                 n for n in self.cell_idx if
-                self.meta["obs"]["SubID"][n] in self.splits[i]["test_subjects"]
+                self.meta["obs"]["SubID"][n] in self.splits_donor[i]["test_subjects"]
             ]
-            self.splits[i]["test_idx"] = idx
+            self.splits[i]["test_idx"] = np.int32(idx)
 
             idx = [
                 n for n in self.cell_idx if
-                self.meta["obs"]["SubID"][n] in self.splits[i]["train_subjects"]
+                self.meta["obs"]["SubID"][n] in self.splits_donor[i]["train_subjects"]
             ]
-            self.splits[i]["train_idx"] = idx
+            self.splits[i]["train_idx"] = np.int32(idx)
 
             print("Size of train subjects/indices, test subjects/indices")
-            print((f"Number of train subjects: {len(self.splits[i]['train_subjects'])}, "
-                   f"Number of test subjects: {len(self.splits[i]['test_subjects'])}, "
+            print((f"Number of train subjects: {len(self.splits_donor[i]['train_subjects'])}, "
+                   f"Number of test subjects: {len(self.splits_donor[i]['test_subjects'])}, "
                    f"Number of train indices: {len(self.splits[i]['train_idx'])}, "
                    f"Number of test indices: {len(self.splits[i]['test_idx'])}"))
 
 
-            idx = set(self.splits[i]["train_subjects"]).intersection(set(self.splits[i]["test_subjects"]))
+            idx = set(self.splits_donor[i]["train_subjects"]).intersection(set(self.splits_donor[i]["test_subjects"]))
             print(f"Intersection size between train and test subjects: {len(idx)}")
 
             idx = set(self.splits[i]["train_idx"]).intersection(set(self.splits[i]["test_idx"]))
